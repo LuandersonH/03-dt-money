d69437ee993dacfae479149c886de0b244dbb91c
LuandersonH <work.luanderson@gmail.com>
2025-01-05 16:47:45 -0300

    Dialog de @radix-ui/react-dialog importado e iniciado no Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,28 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          {/* asChild faz com que o Dialog Trigger não crie um novo botão, mas sim, use o NewTransactionButton em seu lugar.*/}
+          <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+              <Dialog.Overlay/>
+              <Dialog.Content>
+                  <Dialog.Title>Nova transação</Dialog.Title>
+
+                  
+                  <Dialog.Close/>
+              </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
